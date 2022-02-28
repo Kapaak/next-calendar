7a459f968e539496fe686af4ca77cc2cf4388589
Pavel Zapletal <pavel.zaplet25@gmail.com>
2022-02-28 08:53:43 +0100

    dd

--- components/Calendar.jsx
+++ components/Calendar.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import dayjs from "dayjs";
 import locale from "dayjs/locale/cs";
 import weekdayPlugin from "dayjs/plugin/weekday";
-import arrayPlugin from "dayjs/plugin/toObject";
+import objectPlugin from "dayjs/plugin/toObject";
+import isTodayPlugin from "dayjs/plugin/isToday";
 
 const Calendar = () => {
 	const now = dayjs().locale({
@@ -10,7 +11,8 @@ const Calendar = () => {
 	});
 	//so that I start from monday (now its locale aware and cs starts with monday)
 	dayjs.extend(weekdayPlugin);
-	dayjs.extend(arrayPlugin);
+	dayjs.extend(objectPlugin);
+	dayjs.extend(isTodayPlugin);
 
 	const [currentMonth, setCurrentMonth] = useState(now.get("month"));
 	const [selectedData, setSelectedDate] = useState();
@@ -19,11 +21,19 @@ const Calendar = () => {
 	const onDateClick = day => {};
 
 	const nextMonth = () => {
-		setCurrentMonth(prev => prev + 1);
+		const plus = now.month(currentMonth).add(1, "month").month();
+
+		console.log(plus, "plus");
+
+		setCurrentMonth(plus);
 	};
 
 	const prevMonth = () => {
-		setCurrentMonth(prev => prev - 1);
+		const minus = now.month(currentMonth).subtract(1, "month").month();
+
+		console.log(minus, "minus");
+
+		setCurrentMonth(minus);
 	};
 
 	const renderHeader = () => {
@@ -84,14 +94,15 @@ const Calendar = () => {
 		return <div className="body">{rows}</div>;
 	};
 
-	const formateDateObject = dateObject => {
-		const clonedObject = { ...dateObject };
+	const formateDateObject = date => {
+		const clonedObject = { ...date.toObject() };
 
 		const formatedObject = {
 			day: clonedObject.date,
 			month: clonedObject.months,
 			year: clonedObject.years,
-			thisMonth: clonedObject.months === currentMonth,
+			isCurrentMonth: clonedObject.months === currentMonth,
+			isCurrentDay: date.isToday(),
 		};
 
 		return formatedObject;
@@ -99,21 +110,13 @@ const Calendar = () => {
 
 	const getAllDays = () => {
 		let currentDate = now.startOf("month").month(currentMonth).weekday(0);
-		let lastDate = now.endOf("month").month(currentMonth).day(7);
+		const nextMonth = now.month(currentMonth).add(1, "month").month();
+
 		let allDates = [];
 		let weekDates = [];
 
 		let j = 0;
 		let weekCounter = 1;
-
-		// console.log(
-		// 	now.endOf("month").month(currentMonth).weekday(7).toObject(),
-		// 	"last date"
-		// );
-		// console.log(
-		// 	now.endOf("month").month(currentMonth).weekday(6).toObject(),
-		// 	"last date rly"
-		// );
 
 		const pushWeekDates = () => {
 			allDates.push({ dates: weekDates });
@@ -121,10 +124,10 @@ const Calendar = () => {
 			weekCounter = 0;
 		};
 
-		// pokud zacatek tydne neni uz jinej mesic, nez je vybranej, pak vim, ze uz nemam zobrazit
 		// kdyz prvni den tydne dostanu element s mesicem vetsim, nez je ten nynejsi, pak nezobrazuji
-		while (currentDate.weekday(0).toObject().months !== currentMonth + 1) {
-			const formated = formateDateObject(currentDate.toObject());
+		// protoze tim padem to uz je novej mesic a ja zobrazuji elementy jen do konce tydne meho mesice
+		while (currentDate.weekday(0).toObject().months !== nextMonth) {
+			const formated = formateDateObject(currentDate);
 
 			weekDates.push(formated);
 
@@ -136,31 +139,6 @@ const Calendar = () => {
 			weekCounter++;
 			currentDate = now.startOf("month").month(currentMonth).weekday(j);
 		}
-
-		// while (currentDate <= lastDate) {
-		// 	console.log(currentDate.weekday(0).toObject().months);
-
-		// 	// if (j % 7 === 0) console.log(currentDate.toObject().months, "mon");
-		// 	//ta podminka ale ma bejt nad timhle .. nejde pouzit for loop ?
-
-		// 	// co ale upravit jen lastDate a pokud firstDate je pondeli 1. , pak vypisu last date o tyden posunuty ... a pak bude i ta podminka logictejsi
-
-		// 	const formated = formateDateObject(currentDate.toObject());
-
-		// 	weekDates.push(formated);
-
-		// 	//kdyz mesic zacina 1 na pondeli, tak to neprojde posledni tyden
-		// 	//dej tam podminku, ktera kdyz je prvni den pondeli 1. jeste zkoukne, jestli v +1 elementu je ten stejnej mesic
-		// 	//pokud je, tak vypis celej tyden jeste
-
-		// 	if (weekCounter === 7) {
-		// 		pushWeekDates();
-		// 	}
-
-		// 	j++;
-		// 	weekCounter++;
-		// 	currentDate = now.startOf("month").month(currentMonth).weekday(j);
-		// }
 
 		setArrayOfDays(allDates);
 	};
